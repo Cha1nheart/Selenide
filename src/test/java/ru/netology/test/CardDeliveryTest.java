864ef9504923e01ca29cdbf14bbd45ffe5be5cf6
Cha1nHeart <humanoverrated@gmail.com>
2023-06-18 14:41:03 +0500

    Test undated

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -18,16 +18,16 @@ class CardDeliveryTest {
     void shouldAcceptCardDelivery() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Уфа");
-        $("[data-test-id='date'] input").setValue("11.11.11");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(generateDate(7, "dd.MM.yyyy"));
+        String setDate = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(setDate);
         $("[data-test-id='name'] input").setValue("Иванов-Петров Иван");
         $("[data-test-id='phone'] input").setValue("+79991112233");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("[data-test-id='notification']").
                 shouldBe(Condition.visible, Duration.ofSeconds(15)).
-                shouldBe(Condition.text("Встреча успешно забронирована на "));
+                shouldBe(Condition.text("Встреча успешно забронирована на " + setDate));
     }
 
 }
